4f60ba7cf54f6ed1abc2f6b8e22e0fce9915153a
ChoiJinong <cjw4767@naver.com>
2023-09-01 01:06:30 +0900

    [Feat] 게임매니저 구현
    1. 게임 매니저 구현
    2. 플레이어 컨트롤러 스크립트 수정
    3. 스크롤링 오브젝트 스크립트 수정

--- Uni-Run/Assets/Scripts/ScrollingObject.cs
+++ Uni-Run/Assets/Scripts/ScrollingObject.cs
@@ -8,6 +8,7 @@ public class ScrollingObject : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (!GameManager.instance.isGameover)
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 }
